0acd24c1dccbcee4260908c05bc2cdfa10b3cd62
thomasmknight <119625083+thomasmknight@users.noreply.github.com>
2023-02-28 13:54:12 -0600

    Update mapanimation.js

--- mapanimation.js
+++ mapanimation.js
@@ -1,3 +1,13 @@
+var colArray = [
+    "#C41E3A",
+    "#CC5500",
+    "#FFDB58",
+    "#228B22",
+    "#000080",
+    "7F00FF"
+];
+var busToggle = 0;
+
 //your token goes below in place of mine
 mapboxgl.accessToken = 'pk.eyJ1IjoidGhvbWFzbWtuaWdodCIsImEiOiJjbGVuOW5ya28xY3NnM3RyMDQ2ZnNkYWozIn0.ecaejiZKLI8krjs8N6yIxA';
 
@@ -15,14 +25,32 @@ async function addMarkers(){
     var json = await response.json();
     var locations = await json.data;
     console.log(locations);
-    var lat = locations[0].attributes.latitude;
-    var long = locations[0].attributes.longitude;
+    var lat = locations[busToggle].attributes.latitude;
+    var long = locations[busToggle].attributes.longitude;
     console.log(lat);
     console.log(long);
-    var marker = new mapboxgl.Marker()
-        .setLngLat([long, lat])
+    var marker = new mapboxgl.Marker({
+        color: colArray[busToggle],
+        draggable: true
+    }).setLngLat([long, lat])
         .addTo(map);
     map.flyTo({
             center: [long, lat]
         });
+    markers.push(marker);
     }
+
+async function toggleBus() {
+    var url = "https://api-v3.mbta.com/vehicles?api_key=ca34f7b7ac8a445287cab52fb451030a&filter[route]=1&include=trip";
+    var response = await fetch(url);
+    var json = await response.json();
+    var locations = await json.data;
+    if(busToggle <= locations.length){
+    busToggle +=1;
+    } else{
+        busToggle = 0;
+    }
+    addMarkers();
+
+}
+
